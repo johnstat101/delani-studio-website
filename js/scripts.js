f2511ac76709886712e6cea83cb50e3efdefd984
johnstat101 <johnjorogek@gmail.com>
2021-10-25 14:54:53 +0300

    add each portfolio project name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// what we do section.
+// toggle effect function on "what-We-Do" section
 $(document).ready(function() {
     $("#design").click(function() {
         $("#design-icon").toggle();
@@ -11,9 +11,22 @@ $(document).ready(function() {
     $("#product").click(function() {
         $("#product-icon").toggle();
         $("#product-text").toggle();
-    })
+    });
 });
+
 
 // Portfolio section
 
+
 // User Inputs form section
+$(document).ready(function() {
+    $("form").submit(function() {
+        var name = $("input#user-name").val();
+        var email = $("input#user-email").val();
+        var message = $("input#user-message").val();
+        if (name != "" && email != "" && message != "") {
+            alert("Hello " + name + " we have received your message. Thank you for reaching out to us!");
+        };
+        event.preventDefault();
+    });
+});
