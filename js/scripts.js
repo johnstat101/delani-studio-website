7ac66c012cfe150b7db55b1b28f8f72ab347d770
johnstat101 <johnjorogek@gmail.com>
2021-10-25 12:56:28 +0300

    add jQuery code to enable toggle effect on What-We-Do section

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// what we do section.
+$(document).ready(function() {
+    $("#design").click(function() {
+        $("#design-icon").toggle();
+        $("#design-text").toggle();
+    });
+    $("#development").click(function() {
+        $("#development-icon").toggle();
+        $("#development-text").toggle();
+    });
+    $("#product").click(function() {
+        $("#product-icon").toggle();
+        $("#product-text").toggle();
+    })
+});
+
+// Portfolio section
+
+// User Inputs form section
